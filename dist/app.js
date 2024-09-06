11d41714db0c51eff4852764f8a78d1fb776f1e4
Jillder <jordan0968300@gmail.com>
2024-09-05 23:37:23 -0500

    fake rest api

---
+++ dist/app.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const archivos_1 = require("./archivos");
+archivos_1.canchas.forEach(elemento => {
+    console.log(elemento);
+});
---
+++ src/archivos/index.ts
@@ -0,0 +1,6 @@
+export {canchas, deportistas, separaciones} from './data';
+export {recorrerFE} from './recorrerFE';
+export {buscar} from './busqueda';
+export {buscarPromises} from './busquedaPromise';
+export {buscarAsyncAwait} from './busquedaAsyncAwait';
+export {Infpost, fetchData} from './post';
